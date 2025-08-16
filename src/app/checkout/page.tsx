a4dab7a830725a36fabcafc301119093114086ad
Kiriao <48480973+Kiriao@users.noreply.github.com>
2025-08-16 14:59:36 +0800

    plan to sync with firebase (hopefully this fixed it)

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,6 +1,8 @@
 // src/app/checkout/page.tsx
+"use client"; // ✅ make this a client component
+
 import CheckoutClient from "./CheckoutClient";
 
 export default function CheckoutPage() {
-  return <CheckoutClient />; // no props needed
+  return <CheckoutClient />;
 }
