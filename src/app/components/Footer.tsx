536a047fc1c9e86138c9902caa61309cba18a0bd
Kiriao <48480973+Kiriao@users.noreply.github.com>
2025-09-28 22:35:16 +0800

    Revamp Landing Page add Demo button for future

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-pink-600 text-white text-center py-6 pt-6">
+    <footer className="bg-white text-gray-900 text-center py-6 pt-6">
       <p>© 2025 KidFlix. All rights reserved.</p>
       <div className="mt-2 space-x-4">
         <a href="#" className="hover:underline">Privacy Policy</a>
